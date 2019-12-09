eee8f6f35003fb09847d0e7bddbfd9db0e461536
Ardit Maloku <am42690@ubt-uni.net>
2019-12-09 21:21:49 +0100

    auth guard

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav>
+        <div class="nav-wrapper green">
+            <div class="container">
+                <router-link to="/" class="brand-logo">Employee Manager</router-link>
+                <ul class="right">
+                    <li v-if="isLoggedIn"><span class="email black-text">{{currentUser}}</span></li>
+                    <li><router-link v-if="isLoggedIn" to="/">Dashboard</router-link></li>
+                    <li><router-link v-if="!isLoggedIn" to="/login">Login</router-link></li>
+                    <li><router-link v-if="!isLoggedIn" to="/register">Register</router-link></li>
+                    <li v-if="isLoggedIn"><button v-on:click="logout" class="btn black">Logout</button></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+    name: 'navbar',
+    data () {
+        return {
+            isLoggedIn: false,
+            currentUser: false
+        };
+    },
+    created () {
+        if(firebase.auth().currentUser)
+        {
+            this.isLoggedIn = true;
+            this.currentUser = firebase.auth().currentUser.email;
+        }
+    },
+    methods: {
+        logout: function() {
+            firebase.auth().signOut().then(() => {
+            this.$router.go({path: this.$router.path})
+            })
+        }
+    }
+}
+</script>
+
+<style>
+.email{
+    padding-left: 10px;
+}
+</style>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyAP6xVecZkWpFeAbHCllJgvWEuAIeOfOvw",
+    authDomain: "webdevelopment-d4670.firebaseapp.com",
+    databaseURL: "https://webdevelopment-d4670.firebaseio.com",
+    projectId: "webdevelopment-d4670",
+    storageBucket: "webdevelopment-d4670.appspot.com",
+    messagingSenderId: "818878255698",
+    appId: "1:818878255698:web:87ce04479926205ca4458b",
+    measurementId: "G-YTE6JF10JL"
+}
